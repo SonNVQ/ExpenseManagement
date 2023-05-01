e88ae84639ec8107421044771b78be3544e1d013
Nguyen Son <50987070+SonNVQ@users.noreply.github.com>
2023-05-02 02:56:40 +0700

    Optimize imports

--- backend/src/main/java/lab/ia/ExpenseManagement/Payloads/Response/UserResponse.java
+++ backend/src/main/java/lab/ia/ExpenseManagement/Payloads/Response/UserResponse.java
@@ -1,11 +1,7 @@
 package lab.ia.ExpenseManagement.Payloads.Response;
 
-import lab.ia.ExpenseManagement.Models.Enums.ERole;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-
-import java.util.List;
-import java.util.Set;
 
 @Data
 @AllArgsConstructor
