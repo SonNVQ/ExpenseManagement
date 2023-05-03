6bfd3794893af67893f04e03f78e458c76e018b2
Nguyen Son <50987070+SonNVQ@users.noreply.github.com>
2023-05-03 23:14:29 +0700

    Optimize imports

--- backend/src/main/java/lab/ia/ExpenseManagement/Services/AuthService.java
+++ backend/src/main/java/lab/ia/ExpenseManagement/Services/AuthService.java
@@ -3,7 +3,6 @@ package lab.ia.ExpenseManagement.Services;
 import lab.ia.ExpenseManagement.Payloads.Request.LoginRequest;
 import lab.ia.ExpenseManagement.Payloads.Request.RegisterRequest;
 import lab.ia.ExpenseManagement.Payloads.Response.JwtResponse;
-import org.springframework.security.authentication.BadCredentialsException;
 
 public interface AuthService {
     JwtResponse login(LoginRequest loginRequest);
