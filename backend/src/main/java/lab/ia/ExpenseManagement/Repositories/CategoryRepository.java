d66418b85678b8bc151f476c738125e5aaa76482
Nguyen Son <50987070+SonNVQ@users.noreply.github.com>
2023-05-03 23:14:19 +0700

    Add Category REST Controller

--- backend/src/main/java/lab/ia/ExpenseManagement/Repositories/CategoryRepository.java
+++ backend/src/main/java/lab/ia/ExpenseManagement/Repositories/CategoryRepository.java
@@ -1,9 +1,15 @@
 package lab.ia.ExpenseManagement.Repositories;
 
 import lab.ia.ExpenseManagement.Models.Category;
+import lab.ia.ExpenseManagement.Models.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Page<Category> findCategoriesByUser(User user, Pageable pageable);
+
+    Page<Category> findAllByUser(User user, Pageable pageable);
 }
