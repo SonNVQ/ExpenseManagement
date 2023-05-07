0a108c0aec6f8e5d7d4b456fa5027b552fafc4ec
loggjn <86997341+loggjn@users.noreply.github.com>
2023-05-07 19:21:07 +0700

    Commit

--- backend/src/main/java/lab/ia/ExpenseManagement/Controllers/uploadFileController.java
+++ backend/src/main/java/lab/ia/ExpenseManagement/Controllers/uploadFileController.java
@@ -1,5 +1,6 @@
 package lab.ia.ExpenseManagement.Controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lab.ia.ExpenseManagement.Payloads.Request.RecordRequest;
 import lab.ia.ExpenseManagement.Payloads.Response.MessageResponse;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -27,18 +29,18 @@ public class uploadFileController {
     @PostMapping("/records")
     public ResponseEntity<?> fileUpload(@CurrentUser UserPrincipal currentUser
                                         ,@RequestBody String file
-                                         )    {
+                                         ) throws JsonProcessingException {
         //create ObjectMapper instance
         ObjectMapper objectMapper = new ObjectMapper();
 
 
 
         //read JSON file and convert to a customer object
-//        List<RecordRequest> recordLi = List.of(objectMapper.readValue( file, RecordRequest[].class));
+          
 //        System.out.println(file+" asd ================== asd");
 
         try {
-            List<RecordRequest> recordList = List.of(objectMapper.readValue( file, RecordRequest[].class));
+            List<RecordRequest> recordList = List.of(objectMapper.readValue( file, RecordRequest.class));
             for (RecordRequest recordRequest : recordList){
                 recordServer.addRecord( recordRequest, currentUser);
 
